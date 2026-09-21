40b80e235684b075a6a3b2888b19e7d3837dd120
andyyeah-debug <marianaandreamoraleslopez@gmail.com>
2026-09-20 23:03:37 -0600

    Update script.js

--- script.js
+++ script.js
@@ -656,7 +656,7 @@ Te ama, Mariana<3`
 
 
                 temporizadorCarta =
-                    setTimeout(escribir, 10);
+                    setTimeout(escribir, 60);
 
             } else {
 
